9e9ccfe9a58674e8e9a0cff351be5471d22c8779
Eugeniusz W <eugwas@gmail.com>
2021-10-26 18:14:40 +0200

    Scanner was added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,22 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Greatest common divisor = " + GreatestCommonDivisor.getGreatestCommonDivisor(81, 153));
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        boolean hasNextInt = scanner.hasNextInt();
+        if (hasNextInt) {
+            int first = scanner.nextInt();
+            System.out.print("Enter the second number: ");
+            int second = scanner.nextInt();
+            System.out.println("Greatest common divisor = " + GreatestCommonDivisor.getGreatestCommonDivisor(first, second));
+        } else {
+            System.out.println("Invalid number");
+        }
+        scanner.nextLine();
+        scanner.close();
     }
 }
